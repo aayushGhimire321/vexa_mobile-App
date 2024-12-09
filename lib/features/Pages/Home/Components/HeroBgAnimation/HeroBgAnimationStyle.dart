6df7dd66bbe520d797b37c6aae17251cd95cfbda
aayushghimire321 <aghimire491@gmail.com>
2024-12-09 20:17:07 +0545

    Working on splash screen

--- lib/features/Pages/Home/Components/HeroBgAnimation/HeroBgAnimationStyle.dart
+++ lib/features/Pages/Home/Components/HeroBgAnimation/HeroBgAnimationStyle.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 
 class StyledContainer extends StatelessWidget {
+  const StyledContainer({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return const SizedBox(
       width: 500, // Width in logical pixels
       height: 500, // Height in logical pixels
       // Add additional styling here if needed (e.g., color, border)
--- lib/features/Pages/Home/Components/HeroBgAnimation/index.dart
+++ lib/features/Pages/Home/Components/HeroBgAnimation/index.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 
 class HeroBgAnimation extends StatelessWidget {
+  const HeroBgAnimation({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SizedBox(
       width: 500,
       height: 500,
       child: CustomPaint(
