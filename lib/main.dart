0ddcbe3f53375c1b8795f94e16b1c926810f4186
aayushghimire321 <aghimire491@gmail.com>
2024-12-11 17:00:17 +0545

    works going on

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,17 @@
 import 'package:flutter/material.dart';
 
-
 void main() {
-  runApp(const ProjectManagementApp());
+  runApp(MyApp());
 }
 
-class ProjectManagementApp extends StatelessWidget {
-  const ProjectManagementApp({Key? key}) : super(key: key);
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Project Management App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      initialRoute: '/splash',
-      routes: {
-        '/splash': (context) => const SplashPage(),
-        '/dashboard': (context) => const DashboardPage(), // Add Dashboard Page
-      },
+      title: 'Login Register App',
+      theme: AppTheme.darkTheme,
+      home: SplashScreen(),
     );
   }
 }
