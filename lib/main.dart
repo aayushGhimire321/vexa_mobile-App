c4a7e4cf0b62a85f09bba937aa5e536cfe5a69ef
aayushghimire321 <aghimire491@gmail.com>
2024-12-11 19:00:59 +0545

    slight changes

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Login Register App',
+      title: 'Vexa',
       theme: AppTheme.darkTheme,
       home: SplashScreen(),
     );
