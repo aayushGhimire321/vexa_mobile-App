cc942f20ba6e9a2cf34e5fbc39174e838d9bc75e
aayushghimire321 <aghimire491@gmail.com>
2024-12-12 18:42:14 +0545

    Onboarding screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:vexa/screens/splash_screen.dart';
 import 'package:vexa/themes/theme.dart';
+import 'screens/splash_screen.dart';
+import 'screens/onboarding_screen.dart';
+import 'screens/login_screen.dart';
+import 'screens/register_screen.dart'; // Import your registration screen
 
 void main() {
   runApp(MyApp());
@@ -10,10 +13,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Vexa',
+      title: 'Vexa App',
       theme: AppTheme.darkTheme,
-      home: SplashScreen(),
+      initialRoute: '/splash', // Set splash as the initial route
+      routes: {
+        '/splash': (context) => SplashScreen(),
+        '/onboarding': (context) => OnboardingScreen(),
+        '/login': (context) => LoginScreen(),
+        '/register': (context) => RegisterScreen(), // Add registration route
+      },
     );
   }
 }
