016f7dce86db294650f758d1fdbdde3def476af4
aayushghimire321 <aghimire491@gmail.com>
2024-12-12 19:33:02 +0545

    snackbar and logics for login

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -4,6 +4,12 @@ import '../widgets/custom_button.dart';
 import '../widgets/custom_textfield.dart';
 
 class LoginScreen extends StatelessWidget {
+  final String correctUsername = "Aayush";
+  final String correctPassword = "12345";
+
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,16 +46,51 @@ class LoginScreen extends StatelessWidget {
               SizedBox(height: 20),
               CustomTextField(
                 hintText: 'Username',
+                controller: usernameController, // Bind to the username controller
               ),
               SizedBox(height: 10),
               CustomTextField(
                 hintText: 'Password',
                 obscureText: true,
+                controller: passwordController, // Bind to the password controller
               ),
               SizedBox(height: 20),
               CustomButton(
                 text: 'Login',
-                onPressed: () {},
+                onPressed: () {
+                  String username = usernameController.text.trim();
+                  String password = passwordController.text.trim();
+
+                  if (username == correctUsername && password == correctPassword) {
+                    // If credentials are correct
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text(
+                          'Logged in successfully',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        backgroundColor: Colors.green,
+                      ),
+                    );
+
+                    // Navigate to the DashboardScreen
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => DashboardScreen()),
+                    );
+                  } else {
+                    // If credentials are incorrect
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text(
+                          'Invalid credentials',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        backgroundColor: Colors.red,
+                      ),
+                    );
+                  }
+                },
               ),
               SizedBox(height: 10),
               Text(
