6ae9de2ca794c59b0a8500882f45a84011580cb4
aayushghimire321 <aghimire491@gmail.com>
2025-01-08 01:37:00 +0545

    error fixed

--- lib/core/network/hive_service.dart
+++ lib/core/network/hive_service.dart
@@ -5,20 +5,15 @@ import '../../app/constants/hive_table_constant.dart';
 import '../../features/auth/data/model/auth_hive_model.dart';
 
 class HiveService {
-  // Declare and initialize the box name as a constant
   final String userBoxName = HiveTableConstant.userBox;
 
   Future<void> init() async {
-    // Get directory and initialize the Hive database
     var directory = await getApplicationDocumentsDirectory();
     var path = '${directory.path}vexa_project_management.db';
     Hive.init(path);
-
-    // Register adapters
     Hive.registerAdapter(AuthHiveModelAdapter());
   }
 
-  // User Queries
   Future<void> addUser(AuthHiveModel user) async {
     var box = await Hive.openBox<AuthHiveModel>(userBoxName);
     await box.put(user.userId, user);
@@ -32,5 +27,15 @@ class HiveService {
   Future<List<AuthHiveModel>> getAllUsers() async {
     var box = await Hive.openBox<AuthHiveModel>(userBoxName);
     return box.values.toList();
+  }
+
+  Future<AuthHiveModel?> getUserById(String userId) async {
+    var box = await Hive.openBox<AuthHiveModel>(userBoxName);
+    return box.get(userId);
+  }
+
+  Future<void> updateUser(AuthHiveModel user) async {
+    var box = await Hive.openBox<AuthHiveModel>(userBoxName);
+    await box.put(user.userId, user);
   }
 }
