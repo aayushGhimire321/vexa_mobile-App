45bbb851259e0e0a609099ed08906787c1a7f833
Aayush Ghimire <aghimire491@gmail.com>
2025-02-06 09:39:31 +0545

    api works

--- lib/app/constants/api_endpoints.dart
+++ lib/app/constants/api_endpoints.dart
@@ -1,8 +1,8 @@
 class ApiEndpoints {
   ApiEndpoints._();
 
-  static const Duration connectionTimeout = Duration(seconds: 1000);
-  static const Duration receiveTimeout = Duration(seconds: 1000);
+  static const Duration connectionTimeout = Duration(seconds: 10);
+  static const Duration receiveTimeout = Duration(seconds: 10);
   static const String baseUrl = "http://10.0.2.2:3000/api/v1/";
 
   // ====================== Auth Routes ======================
